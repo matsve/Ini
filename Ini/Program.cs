e38333cf46c35cb55c4ca3728fb3ba6e8fd8facf
Svennosven <svennosven@gmail.com>
2014-11-04 23:16:28 +0100

    Initial commit
    The initial commit

---
+++ Ini/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Ini;
+
+namespace Ini
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var ini = new System.Data.Ini.Ini();
+            Console.WriteLine("Welcome to the Ini test program!");
+            Console.ReadKey();
+        }
+    }
+}
